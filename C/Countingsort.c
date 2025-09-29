00d816e0efd3cda4664b27049590632178478068
Krishna Saxena <152802376+KrishnaSaxena108@users.noreply.github.com>
2025-09-29 21:51:29 +0530

    Create Countingsort.c

---
+++ C/Countingsort.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+void countingSort(int arr[], int n) {
+    int output[n];
+    int max = arr[0];
+    
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    int count[max + 1];
+    for (int i = 0; i <= max; i++) {
+        count[i] = 0;
+    }
+
+    for (int i = 0; i < n; i++) {
+        count[arr[i]]++;
+    }
+
+    for (int i = 1; i <= max; i++) {
+        count[i] += count[i - 1];
+    }
+
+    for (int i = n - 1; i >= 0; i--) {
+        output[count[arr[i]] - 1] = arr[i];
+        count[arr[i]]--;
+    }
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = output[i];
+    }
+}
+
+int main() {
+    int arr[] = {4, 2, 2, 8, 3, 3, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    countingSort(arr, n);
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
